150b0dd0896d620650af47787884b5de771227e9
Guvan <vwugbd@163.com>
2022-03-24 21:32:10 +0800

    关注他人，显示关注人动态

---
+++ springbootdemo1/src/main/java/com/example/demo/controller/FollowController.java
@@ -0,0 +1,85 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entity.Comments;
+import com.example.demo.entity.Dynamic;
+import com.example.demo.entity.Follow;
+import com.example.demo.entity.User;
+import com.example.demo.service.DynamicService;
+import com.example.demo.service.FollowService;
+import com.example.demo.service.TopicService;
+import com.example.demo.service.UserService;
+import com.example.demo.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author admin
+ * @since 2022-03-24
+ */
+@RestController
+public class FollowController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private FollowService followService;
+    @Autowired
+    private DynamicService dynamicService;
+    @Autowired
+    private TopicService topicService;
+
+
+
+    /**
+     * 添加关注
+     * @param request
+     * @param email
+     * @return
+     */
+    @GetMapping("/follow")
+    public Map addFollow(HttpServletRequest request,String email){
+        Map param = new HashMap();
+        //得到email
+        String userEmail = JwtUtils.parseEmail( request.getHeader("token"));
+        int i = followService.insertFollow(userEmail,email);
+        if(i==1){
+            param.put("status",200);
+            param.put("msg","关注成功");
+        }else {
+            param.put("status",0);
+            param.put("msg","关注失败");
+        }
+        return param;
+    }
+    @PutMapping("/follow")
+    public Map deleteFollow(HttpServletRequest request,String email){
+        Map param = new HashMap();
+        //得到email
+        String userEmail = JwtUtils.parseEmail( request.getHeader("token"));
+        int i = followService.insertFollow(userEmail,email);
+        if(i==1){
+            param.put("status",200);
+            param.put("msg","关注成功");
+        }else {
+            param.put("status",0);
+            param.put("msg","关注失败");
+        }
+        return param;
+    }
+
+}
+
---
+++ springbootdemo1/src/main/java/com/example/demo/service/FollowService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.demo.entity.Follow;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author admin
+ * @since 2022-03-24
+ */
+
+public interface FollowService extends IService<Follow> {
+    int insertFollow(String uId,String followId);
+    List getFollowByUserEmail(String userEmail);
+}
